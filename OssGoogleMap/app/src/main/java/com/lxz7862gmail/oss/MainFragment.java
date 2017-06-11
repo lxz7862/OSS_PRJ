b507b59f82bff9a06ce356a974fef231a7c20bf1
HAKO-PC\wscom_boh3i3o <wscom28@naver.com>
2017-06-12 03:53:09 +0900

    메인 앱으로 합치고 탭 메뉴에 지도화면 표시

---
+++ OssGoogleMap/app/src/main/java/com/lxz7862gmail/oss/MainFragment.java
@@ -0,0 +1,22 @@
+package com.lxz7862gmail.oss;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.zip.Inflater;
+
+public class MainFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return view;
+    }
+
+}
